0f2fda01607e49571c66589e550a23bd312fb3c7
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-20 18:27:30 -0500

    submitted solution

--- src/reverse.js
+++ src/reverse.js
@@ -4,6 +4,9 @@
  * Returns the string with the characters in reverse order
  * @param {string} text the string to be reversed
  */
-function reverse(text) {}
+function reverse(text) {
+  if (text.length < 2) return text;
+  return reverse(text.substring(1)) + text.charAt(0);
+}
 
 module.exports = reverse;
